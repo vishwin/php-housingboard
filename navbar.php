971e8aa33dfdf9f9eb55814e622349391015a5c3
Charlie Li <git@vishwin.info>
2014-05-05 18:58:17 -0400

    Smart page indicators in navbar

--- navbar.php
+++ navbar.php
@@ -13,11 +13,12 @@
 					<li><a href="logout.php">Log out</a></li>
 				</ul>
 			</li><?php }
-			else { ?><li><a href="login.php">Log in / Create account</a></li><?php } ?>
+			else { ?><li<?php if (basename($_SERVER['REQUEST_URI'])=="login.php") { ?> class="active"<?php } ?>><a href="login.php">Log in / Create account</a></li><?php } ?>
 		</ul>
 		
 		<?php if (isset($_SESSION['user'])) { ?><ul class="left">
-			<li><a href="post.php?action=add">Post</a></li>
+			<li<?php if (basename($_SERVER['REQUEST_URI'])=="post.php?action=add") { ?> class="active"<?php } ?>><a href="post.php?action=add">New post</a></li>
+			<?php if (isset($_GET['id']) && basename($_SERVER['SCRIPT_NAME'])=="post.php" && $db_connection->query('select username from posts where post_id="' . $_GET['id'] . '"')->fetch_assoc()['username']==$_SESSION['user']) { ?><li<?php if (isset($_GET['action']) && $_GET['action']=="modify") { ?> class="active"<?php } ?>><a href="post.php?action=modify&id=<?php echo $_GET['id']; ?>">Edit</a></li><?php } // show edit in navbar if post belongs to logged-in user ?>
 		</ul><?php } ?>
 	</section>
 </nav>
