4933474aab15b64e259cadb07494c028f6504c13
Charlie Li <git@vishwin.info>
2014-05-05 23:12:58 -0400

    Ignore and remove config.php as well

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php // connection info and actions
-$db_host="127.0.0.1";
-$db_username='housingboard';
-$db_password='Fitz and The Tantrums';
-$db_connection=new mysqli($db_host, $db_username, $db_password, "housingboard");
-if ($db_connection->connect_errno) {
-	printf("Cannot connect to database.\n(A)bort, (R)etry, (F)ail?\n");
-}
-?>
